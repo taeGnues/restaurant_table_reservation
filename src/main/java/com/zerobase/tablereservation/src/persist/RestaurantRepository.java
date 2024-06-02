b60a48b6ee98163d1e9133e92308fa0248317052
정태승 <hi563@g.skku.edu>
2024-06-02 20:50:39 +0900

    feat: [006-RESTAURANT-SEARCH] 식당 이름으로 조회하기(검색), 모든 식당 조회하기, 페이징 도입
    - RestaurantRegisterDTO의 필드 변경. Restaurant의 id까지 전달하도록 수정.
    - API 테스트 완료.
    - README UPDATE

--- src/main/java/com/zerobase/tablereservation/src/persist/RestaurantRepository.java
+++ src/main/java/com/zerobase/tablereservation/src/persist/RestaurantRepository.java
@@ -1,8 +1,10 @@
 package com.zerobase.tablereservation.src.persist;
 
 import com.zerobase.tablereservation.src.persist.entity.Restaurant;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
@@ -10,4 +12,6 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     Optional<Restaurant> findByManager_Id(Long id);
 
     void deleteByManager_Id(Long userId);
+
+    List<Restaurant> findAllByNameContains(String keyword, Pageable pageable);
 }
